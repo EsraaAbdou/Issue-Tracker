0243ae7266dafc148d8b2d56f016e6225cb93b7f
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-03-27 04:46:20 +0000

    edit post req

--- routes/api.js
+++ routes/api.js
@@ -6,12 +6,13 @@ mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: tr
 const issueSchema  =  new mongoose.Schema({ 
   issue_title: { type: String, required: true},
   issue_text: { type: String, required: true},
-  created_on: { type: Date, required: true},
-  updated_on: Date,
   created_by: { type: String, required: true},
-  assigned_to: String,
-  open: Boolean,
-  status_text: String
+  assigned_to: { type: String, default: ""},
+  status_text: { type: String, default: "" },
+  open: { type: Boolean, default: true}
+  }, {
+  versionKey: false,
+  timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' }
 });
 const Issue =  mongoose.model('Issue', issueSchema);
 
@@ -21,17 +22,15 @@ module.exports = function (app) {
   
     .get(function (req, res){
       let project = req.params.project;
-      
+
     })
 
     .post(function (req, res){
       let project = req.params.project;
       const  { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
       if(issue_title && issue_text && created_by) {
-        const created_on = new Date();
-        const updated_on = new Date();
         const open = true;
-        const issue = new Issue({ issue_title, issue_text, created_on, updated_on, created_by, assigned_to, open, status_text});
+        const issue = new Issue({issue_title, issue_text, created_by, assigned_to, open, status_text});
         issue.save((err, data) => {
           if(data) res.send(data);
         });
